9a42a3d23cc3c0bf930014244a04f63eb778b9d6
Mani Movassagh <mani.mgh@gmail.com>
2021-03-03 15:57:37 +0100

    4

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 
 const buttons=document.getElementById('one')
 const button2=document.getElementById('two')
+const button3=document.getElementById('three')
 const screen =document.getElementById('screen')
 console.log(screen);
+
+const buttonsAll=document.querySelectorAll(".btn")
+console.log(buttonsAll.values);
 
 const listener=buttons.addEventListener('click',(e)=>{
 screen.innerHTML=e.target.value
 }); 
 const listenerTwo=button2.addEventListener('click',(e)=>{
+    screen.innerHTML=e.target.value
+}); 
+
+const listener3=button3.addEventListener('click',(e)=>{
     screen.innerHTML=e.target.value
 }); 
 
