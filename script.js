d3d9fce0417d888df09fc8abda4ad8802278b901
Mani Movassagh <mani.mgh@gmail.com>
2021-03-03 16:20:55 +0100

    numbers on scrren

--- script.js
+++ script.js
@@ -1,27 +1,19 @@
+const screen = document.getElementById("screen");
+const numberStorage = [];
+const buttonsAll = document.querySelectorAll(".btn");
 
-const buttons=document.getElementById('one')
-const button2=document.getElementById('two')
-const button3=document.getElementById('three')
-const screen =document.getElementById('screen')
-const numberStorage=[]
-const buttonsAll=document.querySelectorAll(".btn")
-
-const ListenToMe=document.addEventListener('click',(e)=>{
-    screen.innerHTML=e.target.value
-   return (e.target.value
-    ) 
-})
-
-
+const ListenToMe = document.addEventListener("click", (e) => {
+  screen.innerHTML = e.target.value;
+  return e.target.value;
+});
 
 // const listener=buttons.addEventListener('click',(e)=>{
 // screen.innerHTML=e.target.value
-// }); 
+// });
 // const listenerTwo=button2.addEventListener('click',(e)=>{
 //     screen.innerHTML=e.target.value
-// }); 
+// });
 
 // const listener3=button3.addEventListener('click',(e)=>{
 //     screen.innerHTML=e.target.value
-// }); 
-
+// });
